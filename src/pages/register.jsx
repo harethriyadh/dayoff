1bab978c0fe20b7270555aa0f097794877dbfdd6
Harith Riyadh <hareth.cn12012@gmail.com>
2025-04-30 13:07:06 +0300

    form work n

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -113,9 +113,9 @@ function RegisterPage() {
                                             required
                                         />
                                         <span className="input-group-text">
-                                            <a
-                                                href="#"
-                                                className="link-secondary password-toggle"
+                                            <button
+                                                type="button"
+                                                className="btn btn-link link-secondary password-toggle p-0"
                                                 title="إظهار كلمة المرور"
                                                 data-bs-toggle="tooltip"
                                                 onClick={togglePasswordVisibility}
@@ -135,7 +135,7 @@ function RegisterPage() {
                                                     <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
                                                     <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
                                                 </svg>
-                                            </a>
+                                            </button>
                                         </span>
                                     </div>
                                 </div>
