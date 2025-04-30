ec52cb0947b0a09b3e45deba89f27ce093f2ea75
Harith Riyadh <hareth.cn12012@gmail.com>
2025-04-30 11:48:42 +0300

    form work n

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -113,30 +113,31 @@ function RegisterPage() {
                                             required
                                         />
                                         <span className="input-group-text">
-                                            <a
-                                                href="#"
-                                                className="link-secondary password-toggle"
-                                                title="إظهار كلمة المرور"
-                                                data-bs-toggle="tooltip"
-                                                onClick={togglePasswordVisibility}
-                                            >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="24"
-                                                    height="24"
-                                                    viewBox="0 0 24 24"
-                                                    fill="none"
-                                                    stroke="currentColor"
-                                                    strokeWidth="2"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    className="icon icon-1"
-                                                >
-                                                    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                                                    <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-                                                </svg>
-                                            </a>
-                                        </span>
+            <button
+              type="button"
+              className="link-secondary password-toggle"
+              title={passwordVisible ? 'إخفاء كلمة المرور' : 'إظهار كلمة المرور'}
+              data-bs-toggle="tooltip"
+              onClick={togglePasswordVisibility}
+              style={{ border: 'none', background: 'transparent', padding: 0, cursor: 'pointer' }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="icon icon-1"
+              >
+                <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+              </svg>
+            </button>
+          </span>
                                     </div>
                                 </div>
 
